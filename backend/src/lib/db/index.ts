c581e22c35e99c94ace819f1bdc31edbeb1315c0
Kunjan Thakkar <kunj.t.2490@gmail.com>
2024-12-18 15:25:53 +0800

    Docker

--- backend/src/lib/db/index.ts
+++ backend/src/lib/db/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/nodechat-app');
+    await mongoose.connect('mongodb://host.docker.internal:27017/webapp');
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection error:', error);
